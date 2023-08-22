f5b820f80aa3bec4ac4dc42383439695dcef1fe4
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2023-08-22 14:48:24 +0100

    got a simple version of fetching working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import Hero from "@/components/sections/hero/Hero";
+import { client } from "@/sanity/lib/client";
+import { postsQuery } from "@/sanity/lib/queries";
 
-export default function Home() {
+export async function getStaticProps() {
+  const posts = await client.fetch("*[_type == 'post']");
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+export default function Home({ posts }: { posts: any }) {
+  console.log(posts);
   return (
     <main>
       <Hero />
